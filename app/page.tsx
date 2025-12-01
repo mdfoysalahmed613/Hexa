7cf526ea65e906170c657ab162ea0b3f74f67630
Md Foysal Ahmed <mdfoysalahmed613@gmail.com>
2025-12-01 18:02:50 +0600

    feat: add shadcn component

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center">
-      <h1 className="text-4xl font-bold mt-10">Welcome to the Home Page</h1>
+      <h1 className="text-4xl font-bold mt-10 text-primary">Welcome to the Home Page</h1>
       <p className="mt-4 text-lg">This is the main landing page of the application.</p>
+      
     </main>
   );
 }
